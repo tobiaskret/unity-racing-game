9758829ded7c221dc3e343698e272b58260e7bbe
Lucas Liess-Duquesne <lucas.ld@hermes-link.de>
2021-04-26 15:46:19 +0200

    added a car for testing

---
+++ Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public GameObject leftWheelObject;
+    public GameObject rightWheelObject;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
+
+public class CarBehaviour : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public Rigidbody rb;  // cars rigitbody
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // processing inputs
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");      
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+                // break if veocity positive and motor negative
+                if (rb.velocity.magnitude > 1 && motor < 0)
+                {
+                    axleInfo.leftWheel.brakeTorque = 500;
+                    axleInfo.rightWheel.brakeTorque = 500;
+                } else
+                {
+                    axleInfo.leftWheel.brakeTorque = 0;
+                    axleInfo.rightWheel.brakeTorque = 0;
+                }
+
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel, axleInfo.leftWheelObject);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel, axleInfo.rightWheelObject);
+        }
+    }
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider, GameObject game_object)
+    {
+        /*if (collider.transform.childCount == 0)
+        {
+            Debug.Log(collider.transform.parent);
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;*/
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        //Debug.Log(position);
+        game_object.transform.position = position;
+        game_object.transform.rotation = rotation; // * Quaternion.Euler(Vector3.right * 90);
+
+    }
+
+}
